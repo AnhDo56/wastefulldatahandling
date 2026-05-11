db4ad8fef93ae98b1fdfd1316e14b65e8f4c256e
Maaritneva <maaritnevalainen@hotmail.com>
2026-05-11 19:29:19 +0300

    Update testing.py

--- testing.py
+++ testing.py
@@ -204,7 +204,7 @@ def save_backup():
 
 
 def main():
-    create_data_file_if_missing() #this to be removed? just leave "login ()". Look the start,I suggest removing this from the beginng/MN
+    create_data_file_if_missing() #this to be removed? just leave "login ()". Look the beginning,I suggest removing it OR option 2/MN
     login()
 
     while True:
